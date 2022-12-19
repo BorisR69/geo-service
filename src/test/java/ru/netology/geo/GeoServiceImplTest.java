eafd819e390790cc05047c6d20a1af085ce1b9a8
BorisR69 <rozov69@gmail.com>
2022-12-19 23:56:54 +0300

    Работа дома 19.12.2022

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,19 +1,32 @@
 package ru.netology.geo;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import ru.netology.entity.Country;
 import ru.netology.entity.Location;
+
 
 class GeoServiceImplTest {
 
-    @ParameterizedTest
-    @ValueSource (strings = {"127.0.0.1", "172.0.32.11", "96.44.183.149"})
-     void byIp(String ip) {
-        GeoServiceImpl geoS = new GeoServiceImpl();
+//    @ParameterizedTest
+//    @ValueSource (strings = {"172.0.32.11"})
+//     void byIp(String ip) {
+//        GeoServiceImpl geoS = new GeoServiceImpl();
+//
+//        Assertions.assertEquals(new Location(null, null, null, 0), geoS.byIp(ip));
+//        Assertions.assertEquals(new Location("Moscow", Country.RUSSIA, "Lenina", 15), geoS.byIp(ip));
+//        Assertions.assertEquals("96.44.183.149", GeoServiceImpl.NEW_YORK_IP);
+//    }
 
-//        Assertions.assertSame(new Location(null, null, null, 0), geoS.byIp("127.0.0.1"));
-        Assertions.assertEquals("172.0.32.11", GeoServiceImpl.MOSCOW_IP);
-        Assertions.assertEquals("96.44.183.149", GeoServiceImpl.NEW_YORK_IP);
+    @Test
+    void byIp() {
+        String ip = "172.0.32.11";
+        Location loc = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        GeoServiceImpl geoS = new GeoServiceImpl();
+        Location loc1 = geoS.byIp(ip);
+
+        Assertions.assertEquals(loc, loc1);
     }
 }
