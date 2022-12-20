db4eeddb2c9c739261778555aa1eed32f07403f4
Boris Rozov work <outsource@list.ru>
2022-12-20 17:44:04 +0300

    Доработка 20.12.2022 на работе

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,32 +1,33 @@
 package ru.netology.geo;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 
-
 class GeoServiceImplTest {
 
-//    @ParameterizedTest
-//    @ValueSource (strings = {"172.0.32.11"})
-//     void byIp(String ip) {
-//        GeoServiceImpl geoS = new GeoServiceImpl();
-//
-//        Assertions.assertEquals(new Location(null, null, null, 0), geoS.byIp(ip));
-//        Assertions.assertEquals(new Location("Moscow", Country.RUSSIA, "Lenina", 15), geoS.byIp(ip));
-//        Assertions.assertEquals("96.44.183.149", GeoServiceImpl.NEW_YORK_IP);
-//    }
+    @ParameterizedTest
+    @ValueSource (strings = {"127.0.0.1", "172.0.32.11", "96.44.183.149"})
+     void byIp(String ip) {
+        Location loc1 = new Location(null, null, null, 0);
+        Location loc2 = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        Location loc3 = new Location("New York", Country.USA, " 10th Avenue",32);
 
-    @Test
-    void byIp() {
-        String ip = "172.0.32.11";
-        Location loc = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
         GeoServiceImpl geoS = new GeoServiceImpl();
-        Location loc1 = geoS.byIp(ip);
+        Location loc = geoS.byIp(ip);
 
-        Assertions.assertEquals(loc, loc1);
+        switch (ip) {
+            case "127.0.0.1":
+                Assertions.assertEquals(loc1, loc);
+                break;
+            case "172.0.32.11":
+                Assertions.assertEquals(loc2, loc);
+                break;
+            case "96.44.183.149":
+                Assertions.assertEquals(loc3, loc);
+                break;
+        }
     }
 }
--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -8,25 +8,32 @@ import ru.netology.entity.Location;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationServiceImpl;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 class MessageSenderImplTest {
 
     @Test
-    void sendRussia (String locale) {
-//        LocalizationServiceImpl location = Mockito.mock(LocalizationServiceImpl.class);
-//        Mockito.when(location.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
-//        GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
-//        Mockito.when(geoService.byIp("172.0.32.11")).thenReturn(new Location("Moscow", Country.RUSSIA,
-//                "Lenina", 15));
-//        MessageSenderImpl messageSend = new MessageSenderImpl(geoService, location);
-//
-//        Assertions.assertEquals("Добро пожаловать", messageSend.send());
-//
-//        Mockito.verify(location);
-//        Mockito.verify(geoService);
+    void sendRussia () {
+        Location locMoc = new Location("Moscow", Country.RUSSIA,
+                "Lenina", 15);
+        GeoServiceImpl geoServiceMoc = Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(geoServiceMoc.byIp("172.123.12.19")).thenReturn(locMoc);
 
+        String hiMoc = "Добро пожаловать";
+        LocalizationServiceImpl locationMoc = Mockito.mock(LocalizationServiceImpl.class);
+        Mockito.when(locationMoc.locale(Country.RUSSIA)).thenReturn(hiMoc);
+
+        MessageSenderImpl messageSend = new MessageSenderImpl(geoServiceMoc, locationMoc);
+
+        Map<String, String> headers = new HashMap < >();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
+
+        try {
+            Assertions.assertEquals("Добро пожаловать", messageSend.send(headers));
+        } catch (NullPointerException np) {
+            System.out.println("Что то пошло не так: " + np.getMessage());
+        }
     }
-
-
 }
